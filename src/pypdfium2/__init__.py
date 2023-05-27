d22f16ca26089e5ff5c606f098650642ee243759
geisserml <geisserml@gmail.com>
2023-05-27 11:53:39 +0200

    Explicitly initialize pdfium via `FPDF_InitLibraryWithConfig()`

--- src/pypdfium2/__init__.py
+++ src/pypdfium2/__init__.py
@@ -10,7 +10,16 @@ from pypdfium2 import raw
 logger = logging.getLogger(__name__)  # FIXME perhaps unnecessary?
 
 # Note: PDFium developers plan changes to the initialisation API (see https://crbug.com/pdfium/1446)
-raw.FPDF_InitLibrary()
+raw.FPDF_InitLibraryWithConfig(
+    raw.FPDF_LIBRARY_CONFIG(
+        version = 4,
+        m_pUserFontPaths = None,
+        m_pIsolate = None,
+        m_v8EmbedderSlot = 0,
+        m_pPlatform = None,
+        m_RendererType = raw.FPDF_RENDERERTYPE_AGG,
+    )
+)
 atexit.register(raw.FPDF_DestroyLibrary)
 
 del atexit, logging
